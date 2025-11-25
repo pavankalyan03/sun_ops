fef7b54df0077e9d212e62ded3de905ab9b8a32c
Pk <pavankalyannew003@gmail.com>
2025-11-25 18:17:22 +0530

    QWERTY

--- stored_procs/migrations/scripts/Client2/V3__client2_update_customers.sql
+++ stored_procs/migrations/scripts/Client2/V3__client2_update_customers.sql
@@ -1,5 +1,3 @@
--- Client2 specific: update customer tier based on revenue
-
 
 CREATE OR REPLACE PROCEDURE {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_UPDATE_CUSTOMERS()
 RETURNS STRING
