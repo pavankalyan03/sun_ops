b49c2b196447ce61a0cc4c4a1c1b72459043b7a2
Pk <pavankalyannew003@gmail.com>
2025-11-25 18:06:24 +0530

    ZXCVB

---
+++ stored_procs/migrations/scripts/Client1/V2__client1_process_payments.sql
@@ -0,0 +1,22 @@
+-- Client1 specific stored procedure
+-- This proc processes payments with Client1 rules
+
+CREATE OR REPLACE PROCEDURE {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_PROCESS_PAYMENTS()
+RETURNS STRING
+LANGUAGE JAVASCRIPT
+AS
+$$
+var sql = `
+    INSERT INTO {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_PAYMENTS
+    SELECT 
+        PAYMENT_ID,
+        AMOUNT,
+        STATUS,
+        CURRENT_TIMESTAMP() AS PROCESSED_AT,
+        '{{ special_flag }}' AS CLIENT_FLAG
+    FROM {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_RAW_PAYMENTS
+`;
+
+snowflake.execute({ sqlText: sql });
+return 'Client1 payments processed using flag: {{ special_flag }}';
+$$;
---
+++ stored_procs/migrations/scripts/Client2/V3__client2_update_customers.sql
@@ -0,0 +1,20 @@
+-- Client2 specific: update customer tier based on revenue
+
+CREATE OR REPLACE PROCEDURE {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_UPDATE_CUSTOMERS()
+RETURNS STRING
+LANGUAGE JAVASCRIPT
+AS
+$$
+var sql = `
+    UPDATE {{ target_database }}.{{ target_schema }}.{{ client_prefix }}_CUSTOMERS
+    SET TIER = 
+        CASE 
+            WHEN TOTAL_REVENUE > {{ tier_gold_threshold }} THEN 'GOLD'
+            WHEN TOTAL_REVENUE > {{ tier_silver_threshold }} THEN 'SILVER'
+            ELSE 'BRONZE'
+        END
+`;
+
+snowflake.execute({ sqlText: sql });
+return 'Client2 customers updated using revenue thresholds.';
+$$;
